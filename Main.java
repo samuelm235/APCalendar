00351b4531c6be9c2e15f9bf12006ef023a3afec
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2025-05-03 21:42:26 +0000

    done

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
+        System.out.println(APCalendar.numberOfLeapYears(2000, 2025));
+        System.out.println(APCalendar.dayOfWeek(1, 5, 2019));
+        System.out.println(APCalendar.dayOfWeek(1, 10, 2019));
     }
 }
